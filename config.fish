d83f96e1cbd13d297017c591f6c47bd51c32c49b
7c00 <i@7c00.cc>
2022-01-07 10:13:39 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
 alias cp="cp --reflink"
-alias curl="curl --compressed"
+alias curl="curlie --compressed"
 alias ls="exa"
 alias cat="bat -p --theme='Solarized (light)'"
 alias btm="btm --network_use_bytes --color default-light --hide_time -b"
