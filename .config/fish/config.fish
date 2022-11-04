490e49f546650f9f18618873d3d2441f5a62424b
7c00 <i@7c00.cc>
2022-11-04 13:45:35 +0800

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 alias cp="cp --reflink"
 alias ls="exa"
 alias cat="bat -p --theme='Monokai Extended Light'"
-alias btm="btm --network_use_bytes --color gruvbox-light --hide_time -b"
+alias btm="btm --network_use_bytes --color default-light --hide_time -b"
