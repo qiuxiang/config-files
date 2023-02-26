5ef8d5487ee964b010d7424b2cae4fca55769dde
7c00 <i@7c00.cc>
2023-02-26 22:46:54 +0800

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 alias cp="cp --reflink"
 alias ls="exa"
-alias cat="bat -p --theme='Monokai Extended Light'"
+alias cat="bat -p --theme=OneHalfLight"
 alias btm="btm --network_use_bytes --color default-light --hide_time -b"
