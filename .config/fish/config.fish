e335fd358af4b1ba881bda29f2bea6a6a344cf53
7c00 <i@7c00.cc>
2023-02-27 11:21:08 +0800

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,3 @@
-alias cp="cp --reflink"
 alias ls="exa"
-alias cat="bat -p --theme=OneHalfLight"
+alias cat="batcat -p --theme=OneHalfLight"
 alias btm="btm --network_use_bytes --color default-light --hide_time -b"
